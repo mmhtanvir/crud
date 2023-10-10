659efeee39da7312e197df6ce8651bc77ef0d6db
mmhtanvir <mmhtanvir1@gmail.com>
2023-10-10 06:39:39 +0600

    first commit

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
-from flask import Flask, url_for, render_template, redirect, request, url_for
+from flask import Flask, url_for, render_template, redirect, request, url_for, flash
 from flask_mysqldb import MySQL
 
 app = Flask(__name__)
+
+SECRET_KEY = '18ee19a518f7d69c424d6893151d3525f9fd9e98986626af'
 
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
@@ -18,6 +20,7 @@ def index():
 @app.route('/insert', methods = ['GET', 'POST'])
 def insert():
     if request.method == "POST":
+        flash("Data Inserted Successfully")
         name = request.form['name']
         email = request.form['email']
         phone = request.form['phone']
