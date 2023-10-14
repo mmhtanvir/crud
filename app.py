6dada72c415b126ac54181904d33e9d26cdf8c12
mmhtanvir <mmhtanvir1@gmail.com>
2023-10-14 10:11:21 +0600

    last commit

--- app.py
+++ app.py
@@ -14,13 +14,14 @@ mysql = MySQL(app)
 
 @app.route('/')
 def index():
+    row = {'key': 'value'}
 
     cur = mysql.connection.cursor()
-    cur.execute("SELECT * FROM crud")
+    cur.execute("SELECT * FROM list")
     data = cur.fetchall()
     cur.close()
 
-    return render_template('index.html', crud = data)
+    return render_template('index.html', list = data, row=row)
 
 
 @app.route('/insert', methods = ['GET', 'POST'])
@@ -32,9 +33,39 @@ def insert():
         phone = request.form['phone']
 
         cur = mysql.connection.cursor()
-        cur.execute("INSERT INTO crud(name, email, phone) VALUES(%s, %s, %s)", (name, email, phone))
+        cur.execute("INSERT INTO list(name, email, phone) VALUES(%s, %s, %s)", (name, email, phone))
         mysql.connection.commit()
         return redirect(url_for('index'))
+    
+
+
+@app.route('/update',methods=['POST','GET'])
+def update():
+
+    if request.method == 'POST':
+        id_data = request.form['id']
+        name = request.form['name']
+        email = request.form['email']
+        phone = request.form['phone']
+        cur = mysql.connection.cursor()
+        cur.execute("""
+               UPDATE list
+               SET name=%s, email=%s, phone=%s
+               WHERE id=%s
+            """, (name, email, phone, id_data))
+        flash("Data Updated Successfully")
+        mysql.connection.commit()
+        return redirect(url_for('index'))
+
+
+@app.route('/delete/<string:id_data>', methods = ['GET', 'POST'])
+def delete(id_data):
+    cur = mysql.connection.cursor()
+    cur.execute("DELETE FROM list WHERE id = %s", (id_data))
+    mysql.connection.commit()
+    return redirect(url_for('index'))
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
